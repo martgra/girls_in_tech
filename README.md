44f62293dca043b303f65ce720410802fb7dd533
Martin Gran <martin.kowalik.gran@accenture.com>
2019-04-25 11:28:28 +0200

    working website

---
+++ README.md
@@ -0,0 +1,25 @@
+# Lett nettside med medfølgende server
+
+## Kom i gang
+
+1. Klone repository lokalt
+2. Åpne rotkatalogen "girls_in_tech"
+3. Kjør følgende kommando for å lage virtuelt miljø: 
+
+'''cmd
+python -m venv venv
+'''
+4. Aktiver virtuelt miljø
+'''cmd 
+venv/Scripts/activate
+'''
+5. Installer flask
+'''cmd
+pip install flask
+'''
+6. Kjør applikasjonen
+'''cmd
+python app.py
+'''
+7. Kontroller at du får tilgang på 127.0.0.1:5000
+8. Cache må slettes hver gang endringer i nettsiden gjøres - anbefaler Firefox og CTRL+Shift+del
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/side2')
+def side2():
+    return render_template("side2.html")
+
+# Forsøk å legge til en egen side. Her kan du kopiere koden rett over.
+# Husk at du må velge en ny adresse inne i @app.route('')
+# Husk også at "funksjonen" må få et nytt navn slik som def side3():
+# Tilslutt må vi lage en "side3.html". Vi må redigere "return" til å returnere denne.
+# Skriv inn koden din rett under:
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
